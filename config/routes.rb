380f7229c548b4abd50ff12bab9736eaa5b88ab2
robinlashae1 <robinlashae1@gmail.com>
2022-05-02 17:16:54 -0400

    troubleshooting routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ post "/signup", to: "users#create"
 get "/me", to: "users#show"
 post "/login", to: "sessions#create"
 delete "/logout", to: "sessions#destroy"
-get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+get "*path", to: "fallback#index", constraints: -> (req) { !req.xhr? && req.format.html? }
 end
